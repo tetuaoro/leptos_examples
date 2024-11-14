16d7fc65916851dcae9c9b130811ba2d46d65005
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-11-14 18:44:30 +0100

    remove: ssr feature

--- leptos-ssr-only/src/main.rs
+++ leptos-ssr-only/src/main.rs
@@ -11,6 +11,7 @@ async fn main() {
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     _ = generate_route_list(|| ());
+    dbg!(server_fn::axum::server_fn_paths().collect::<Vec<_>>());
 
     let app = Router::new().route(
         "/api/*fn_name",
