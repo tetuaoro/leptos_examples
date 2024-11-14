8a124928aca8c4ab061e35be7b45769c167bb417
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-11-14 18:23:05 +0100

    update: only ssr
    - use cargo build --package=leptos-ssr-only --bin=leptos-ssr-only

--- leptos-ssr-only/src/main.rs
+++ leptos-ssr-only/src/main.rs
@@ -1,58 +1,25 @@
-#[cfg(feature = "server")]
 #[tokio::main]
 async fn main() {
     use axum::Router;
     use leptos::prelude::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use leptos::*;
+    use leptos_axum::generate_route_list;
+    #[allow(unused_imports)]
     use leptos_ssr_only::app::*;
 
-    let conf = get_configuration(None).unwrap();
+    let conf = get_configuration(Some("Cargo.toml")).unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
-    let routes = generate_route_list(App);
+    _ = generate_route_list(|| ());
 
-    // build our application with a route
-    let app = Router::new()
-        .leptos_routes(&leptos_options, routes, {
-            let leptos_options = leptos_options.clone();
-            move || shell(leptos_options.clone())
-        })
-        .fallback(leptos_axum::file_and_error_handler(shell))
-        .with_state(leptos_options);
+    let app = Router::new().route(
+        "/api/*fn_name",
+        axum::routing::any(leptos_axum::handle_server_fns),
+    );
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    leptos::logging::log!("listening on http://{}", &addr);
+    logging::log!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
-}
-
-#[cfg(feature = "server")]
-fn shell(options: leptos::prelude::LeptosOptions) -> impl leptos::prelude::IntoView {
-    use leptos::prelude::*;
-    use leptos_meta::*;
-    use leptos_ssr_only::app::*;
-
-    view! {
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <AutoReload options=options.clone() />
-                <HydrationScripts options/>
-                <MetaTags/>
-            </head>
-            <body>
-                <App/>
-            </body>
-        </html>
-    }
-}
-
-#[cfg(not(feature = "server"))]
-fn main() {
-    use leptos_ssr_only::app::*;
-    console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(App);
 }
